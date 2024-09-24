2171bf87d1586cab6aece4d2d0ff417bf4cdd175
Jorge Guerra <j.a.guerra@protonmail.com>
2024-09-24 13:57:05 -0700

    first commit

---
+++ src/api.rs
@@ -0,0 +1,112 @@
+use crate::state::ApiResponse::JsonData;
+use crate::state::{
+    ApiError, ApiResponse, JobSetupStep, NodeLevels, NodeWeights, RyoState, Status,
+    SystemCleaningMaintenance,
+};
+use axum::extract::State;
+use axum::Json;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+pub async fn get_state(State(state): State<Arc<Mutex<RyoState>>>) -> Result<ApiResponse, ApiError> {
+    let ryo_state = state.lock().await;
+    Ok(JsonData(serde_json::to_string(&*ryo_state).unwrap()))
+}
+
+pub async fn update_state(
+    State(state): State<Arc<Mutex<RyoState>>>,
+    Json(new_state): Json<RyoState>,
+) -> Result<ApiResponse, ApiError> {
+    let mut ryo_state = state.lock().await;
+    *ryo_state = new_state;
+    Ok(ApiResponse::Ok)
+}
+
+pub async fn get_job_progress(
+    State(state): State<Arc<Mutex<RyoState>>>,
+) -> Result<ApiResponse, ApiError> {
+    let job_progress = &state.lock().await.job_progress;
+    Ok(JsonData(serde_json::to_string(job_progress).unwrap()))
+}
+
+pub async fn get_node_levels(
+    State(state): State<Arc<Mutex<RyoState>>>,
+) -> Result<ApiResponse, ApiError> {
+    println!("get node levels called");
+    let node_levels = &state.lock().await.node_levels;
+    Ok(JsonData(serde_json::to_string(node_levels).unwrap()))
+}
+
+pub async fn update_node_levels(
+    State(state): State<Arc<Mutex<RyoState>>>,
+    Json(new_state): Json<NodeLevels>,
+) -> Result<ApiResponse, ApiError> {
+    println!("{:?}", new_state);
+    let mut ryo_state = state.lock().await;
+    ryo_state.node_levels = new_state;
+    Ok(ApiResponse::Ok)
+}
+
+pub async fn get_node_weights(
+    State(state): State<Arc<Mutex<RyoState>>>,
+) -> Result<ApiResponse, ApiError> {
+    println!("get node weights called");
+    let node_levels = &state.lock().await.node_weights;
+    Ok(JsonData(serde_json::to_string(node_levels).unwrap()))
+}
+
+pub async fn update_node_weight(
+    State(state): State<Arc<Mutex<RyoState>>>,
+    Json(new_weights): Json<NodeWeights>,
+) -> Result<ApiResponse, ApiError> {
+    let mut ryo_state = state.lock().await;
+    ryo_state.node_weights = new_weights;
+    Ok(ApiResponse::Ok)
+}
+
+pub async fn get_status(
+    State(state): State<Arc<Mutex<RyoState>>>,
+) -> Result<ApiResponse, ApiError> {
+    let node_levels = &state.lock().await.status;
+    Ok(JsonData(serde_json::to_string(node_levels).unwrap()))
+}
+
+pub async fn update_status(
+    State(state): State<Arc<Mutex<RyoState>>>,
+    Json(new_status): Json<Status>,
+) -> Result<ApiResponse, ApiError> {
+    let mut ryo_state = state.lock().await;
+    ryo_state.status = new_status;
+    Ok(ApiResponse::Ok)
+}
+
+pub async fn get_sys_clean_maint(
+    State(state): State<Arc<Mutex<RyoState>>>,
+) -> Result<ApiResponse, ApiError> {
+    let sys_cleaning_maint = &state.lock().await.system_cleaning_maintenance;
+    Ok(JsonData(serde_json::to_string(sys_cleaning_maint).unwrap()))
+}
+
+pub async fn update_sys_cleaning_maintenance(
+    State(state): State<Arc<Mutex<RyoState>>>,
+    Json(new_system_cleaning_maintenance): Json<SystemCleaningMaintenance>,
+) -> Result<ApiResponse, ApiError> {
+    let mut ryo_state = state.lock().await;
+    ryo_state.system_cleaning_maintenance = new_system_cleaning_maintenance;
+    Ok(ApiResponse::Ok)
+}
+pub async fn get_job_setup(
+    State(state): State<Arc<Mutex<RyoState>>>,
+) -> Result<ApiResponse, ApiError> {
+    let job_setup_steps = &state.lock().await.job_setup_step;
+    Ok(JsonData(serde_json::to_string(job_setup_steps).unwrap()))
+}
+
+pub async fn update_job_setup(
+    State(state): State<Arc<Mutex<RyoState>>>,
+    Json(new_job_setup): Json<JobSetupStep>,
+) -> Result<ApiResponse, ApiError> {
+    let mut ryo_state = state.lock().await;
+    ryo_state.job_setup_step = new_job_setup;
+    Ok(ApiResponse::Ok)
+}
---
+++ src/state.rs
@@ -0,0 +1,234 @@
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+use serde::{Deserialize, Serialize};
+// #[derive(Serialize)]
+// pub struct Message {
+//     pub(crate) message: String,
+// }
+pub enum ApiResponse {
+    Ok,
+    Created,
+    JsonData(String),
+}
+
+impl IntoResponse for ApiResponse {
+    fn into_response(self) -> Response {
+        match self {
+            Self::Ok => StatusCode::OK.into_response(),
+            Self::Created => StatusCode::CREATED.into_response(),
+            Self::JsonData(data) => (StatusCode::OK, data).into_response(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum ApiError {
+    BadRequest,
+    Forbidden,
+    Unauthorised,
+    InternalServerError,
+}
+
+impl IntoResponse for ApiError {
+    fn into_response(self) -> Response {
+        // Convert your ApiError into an appropriate HTTP response
+        // For example:
+        match self {
+            ApiError::BadRequest => ApiError::BadRequest.into_response(),
+            ApiError::Forbidden => ApiError::Forbidden.into_response(),
+            ApiError::Unauthorised => ApiError::Unauthorised.into_response(),
+            ApiError::InternalServerError => ApiError::InternalServerError.into_response(),
+        }
+    }
+}
+#[derive(Serialize, Deserialize, Debug, Default, Clone)]
+#[serde(rename_all = "PascalCase")]
+pub struct RyoState {
+    pub job_progress: usize,
+    pub job_qty: usize,
+    pub node_levels: NodeLevels,
+    pub node_weights: NodeWeights,
+    pub status: Status,
+    pub system_cleaning_maintenance: SystemCleaningMaintenance,
+    #[serde(rename = "jobSetupStep")]
+    pub job_setup_step: JobSetupStep,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct JobProgress {
+    job_progress: usize,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, Clone)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum NodeLevel {
+    #[default]
+    Low,
+    Med,
+    Loaded,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct NodeLevels {
+    node_a_level: NodeLevel,
+    node_a_level_ingredient_id: String,
+    node_a_level_ingredient_name: String,
+    node_b_level: NodeLevel,
+    node_b_level_ingredient_id: String,
+    node_b_level_ingredient_name: String,
+    node_c_level: NodeLevel,
+    node_c_level_ingredient_id: String,
+    node_c_level_ingredient_name: String,
+    node_d_level: NodeLevel,
+    node_d_level_ingredient_id: String,
+    node_d_level_ingredient_name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct NodeWeights {
+    node_a_weight: u32,
+    node_b_weight: u32,
+    node_c_weight: u32,
+    node_d_weight: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+pub enum SystemStatus {
+    PauseJob,
+    RunJob,
+    RunningJob,
+    ReadyToStartJob,
+    CancelJob,
+    ResumeJob,
+    #[default]
+    StopSystem,
+    PreparingJob,
+    RefillNodesSystem,
+    Faulted,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+#[serde(rename_all = "UPPERCASE")]
+enum DoorStatus {
+    #[default]
+    Open,
+    Close,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct Status {
+    front_door_status: String,
+    system_status: SystemStatus,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct SystemCleaningMaintenance {
+    bag_management: String,
+    clean_mode: String,
+    front_door_control: String,
+    side_door_control: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Default, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct StepDetails {
+    confirm_configuration: String,
+    load_detect: bool,
+    position: i32,
+    status: bool,
+    step_description: String,
+    step_id: String,
+    step_tutorial: String,
+    title: String,
+    visible_check: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize, Default, Clone)]
+#[serde(rename_all = "UPPERCASE")]
+pub struct JobSetupStep {
+    bag_feeder: StepDetails,
+    conveyors: StepDetails,
+    conveyor_lids: StepDetails,
+    remove_all_ingredient_sliders: StepDetails,
+    take_system_photo: StepDetails,
+    tunnels: StepDetails,
+}
+
+// mod tests {
+//     use crate::state;
+//
+//     use crate::state::{NodeLevel, NodeLevels, NodeWeights, Status, SystemStatus};
+//
+//     #[test]
+//     fn test_node_levels() {
+//
+//         use serde_json::json;
+//         let levels = NodeLevels {
+//             node_a_level: NodeLevel::Low,
+//             node_a_level_ingredient_id: "1".to_string(),
+//             node_b_level: NodeLevel::Low,
+//             node_b_level_ingredient_id: "2".to_string(),
+//             node_c_level: NodeLevel::Low,
+//             node_c_level_ingredient_id: "3".to_string(),
+//             node_d_level: NodeLevel::Low,
+//             node_d_level_ingredient_id: "4".to_string(),
+//         };
+//
+//         let levels_json = json!({
+//         "nodeALevel": "LOW",
+//         "nodeALevelIngredientId": "1",
+//         "nodeBLevel": "LOW",
+//         "nodeBLevelIngredientId": "2",
+//         "nodeCLevel": "LOW",
+//         "nodeCLevelIngredientId": "3",
+//         "nodeDLevel": "LOW",
+//         "nodeDLevelIngredientId": "4"
+//     });
+//
+//         let levels_conv = serde_json::to_string(&levels).unwrap();
+//         assert_eq!(levels_json.to_string(), levels_conv);
+//     }
+//
+//     #[test]
+//     fn test_node_weights() {
+//         use serde_json::json;
+//         let weights = NodeWeights {
+//             node_a_weight: 200,
+//             node_b_weight: 300,
+//             node_c_weight: 400,
+//             node_d_weight: 500,
+//         };
+//         let weights_json = json!({
+//         "nodeAWeight": 200,
+//         "nodeBWeight": 300,
+//         "nodeCWeight": 400,
+//         "nodeDWeight": 500,
+//     });
+//
+//         let levels_conv = serde_json::to_string(&weights).unwrap();
+//         assert_eq!(weights_json.to_string(), levels_conv);
+//     }
+//
+//     #[test]
+//     fn test_system_status() {
+//         use state::DoorStatus;
+//         use serde_json::json;
+//         let status = Status {
+//             front_door_status: DoorStatus::Open,
+//             system_status: SystemStatus::PauseJob,
+//         };
+//         let status_json = json!({
+//         "frontDoorStatus": "OPEN",
+//         "systemStatus": "PauseJob",
+//     });
+//         assert_eq!(
+//             status_json.to_string(),
+//             serde_json::to_string(&status).unwrap()
+//         );
+//     }
+// }
